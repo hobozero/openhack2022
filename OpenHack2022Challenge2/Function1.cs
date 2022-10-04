6f68b77e9f021d978675a0e740aeb27220696126
LAPTOP-MD2N11QH\amial <tyler.ulirch@gmail.com>
2022-10-04 12:23:49 -0700

    initial commit

---
+++ OpenHack2022Challenge2/Function1.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OpenHack2022Challenge2
+{
+    public static class Function1
+    {
+        [FunctionName("echo")]
+        public static async Task<IActionResult> RunName(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+           string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"The product name for your product id {name} is Starfruit Explosion and the description is This starfruit ice cream is out of this world!";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        //[FunctionName("productId")]
+        //public static async Task<IActionResult> RunProductId(
+        //    [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+        //    ILogger log)
+        //{
+        //    log.LogInformation("C# HTTP trigger function processed a request.");
+
+        //    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        //    dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+        //    var prodId = data?.productId;
+
+        //    string responseMessage = string.IsNullOrEmpty(prodId)
+        //        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        //        : $"The product name for your product id {prodId} is Starfruit Explosion";
+
+        //    return new OkObjectResult(responseMessage);
+        //}
+
+    }
+}
